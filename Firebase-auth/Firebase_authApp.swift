2d23c56c572333bd358172116f2be67b8c3b75c7
YoucefMac <youcefbou1998@gmail.com>
2023-02-28 11:40:38 +0100

    finish Firebase setup

---
+++ Firebase-auth/Firebase_authApp.swift
@@ -0,0 +1,30 @@
+//
+//  Firebase_authApp.swift
+//  Firebase-auth
+//
+//  Created by apple on 28/2/2023.
+//
+
+import SwiftUI
+import Firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct Firebase_authApp: App {
+    //Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
