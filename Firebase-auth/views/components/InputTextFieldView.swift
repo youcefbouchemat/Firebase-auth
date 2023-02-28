c1809e8de44e09d24424887693b8fef6f57debb6
YoucefMac <youcefbou1998@gmail.com>
2023-02-28 17:42:59 +0100

    modifiy InputTextField

--- Firebase-auth/views/components/InputTextFieldView.swift
+++ Firebase-auth/views/components/InputTextFieldView.swift
@@ -10,27 +10,33 @@ import SwiftUI
 struct InputTextFieldView: View {
     @Binding var text: String
     let placeholder: String
-    let icon: String
+    var icon: String = ""
     let keyboardType: UIKeyboardType
+    
+    @State private var isFocused:Bool = false
+    
     
     var body: some View {
         HStack {
-            
             if(!icon.isEmpty)
             {
                 Image(systemName: icon)
             }
-            TextField(placeholder, text: $text)
+           
+            TextField(placeholder, text: $text,onEditingChanged: { focus in
+                isFocused = focus;
+            })
                 .keyboardType(keyboardType)
         }
         .padding()
-        .overlay(RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 1).foregroundColor(Color.gray))
+        .overlay(RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 1).foregroundColor( isFocused ? Color.blue : Color.gray))
     
     }
 }
 
 struct InputTextFieldView_Previews: PreviewProvider {
     static var previews: some View {
-        InputTextFieldView(text: .constant(""), placeholder: "Email", icon: "lock", keyboardType: .emailAddress)
+        InputTextFieldView(text: .constant(""), placeholder: "Email", keyboardType: .emailAddress)
+        
     }
 }
